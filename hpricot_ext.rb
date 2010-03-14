708063bb704bb87a834d9e87c495d47f3bdc1030
linux_china <linux_china@hotmail.com>
2010-03-14 21:45:16 +0800

    add more method

--- hpricot_ext.rb
+++ hpricot_ext.rb
@@ -1,8 +1,5 @@
 # framework's website: http://wiki.github.com/hpricot/hpricot
 def register_hpricot_types()
-  describe 'Hpricot' do
-    set_return_type "make" => "Hpricot::Doc"
-  end
 
   describe 'Hpricot::Traverse' do
     set_return_type "search" => "Hpricot::Elements"
@@ -10,9 +7,17 @@ def register_hpricot_types()
   end
 
   describe 'Hpricot::Elements' do
+    set_dynamic_methods %w(each first any? length)
     set_return_type 'at' => 'Hpricot::Elem'
+    set_return_type 'first' => 'Hpricot::Elem'
     set_return_type 'search' => 'Hpricot::Elements'
+    set_block_variable_type "each" => "Hpricot::Elem"
   end
+
+  describe 'Hpricot::Elem' do
+    set_return_type "attributes" => "Hpricot::Attributes"
+  end
+
 end
 
 register_hpricot_types
